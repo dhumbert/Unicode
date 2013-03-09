4acad98c3c2115bb673afb2142bc5af65f6f4cd4
Devin Humbert <devin.humbert@gmail.com>
2013-03-09 13:58:59 -0800

    Got 4 byte encoding working and printing the character

--- unicode.c
+++ unicode.c
@@ -1,55 +1,40 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void printbitssimple(int n);
-
 int main() 
 {
-    int value = 0x20AC, hex_value = 0x0;
+    int value = 0x24B62, hex_value = 0x0;
 
     int bytes[4] = {-1, -1, -1, -1};
 
     if (value >= 0x0000 && value <= 0x007F) {
         bytes[0] = value; // characters below code point 127 are ASCII
-    } else if (value >= 0x0080 && value <= 0x07FF) {
+    } else if (value >= 0x0080 && value <= 0x07FF) { // 2 byte encoding
         bytes[0] = 0xC0 | ((value & 0x7C0) >> 6);
         bytes[1] = 0x80 | (value & 0x3F);
-    } else if (value >= 0x0800 && value <= 0xFFFF) {
+    } else if (value >= 0x0800 && value <= 0xFFFF) { // 3 byte encoding
         bytes[0] = 0xE0 | ((value & 0xF000) >> 12);
         bytes[1] = 0x80 | ((value & 0xFC0) >> 6);
         bytes[2] = 0x80 | (value & 0x3F);
-    } else if (value >= 0x10000 && value <= 0x1FFFFF) {
-        printf("3");
-    } else if (value >= 0x200000 && value <= 0x3FFFFFF) {
-        printf("4");
-    } else if (value >= 0x4000000 && value <= 0x7FFFFFFF) {
-        printf("5");
-    } else {
-        printf("6");
+    } else if (value >= 0x10000 && value <= 0x1FFFFF) { // 4 byte encoding
+        bytes[0] = 0xF0 | ((value & 0x1C0000) >> 18);
+        bytes[1] = 0x80 | ((value & 0x3F000) >> 12);
+        bytes[2] = 0x80 | ((value & 0xFC0) >> 6);
+        bytes[3] = 0x80 | (value & 0x3F);
     }
 
-    printf("Hex bytes: ");
     int i;
+    char byte_chars[5] = {'\0', '\0', '\0', '\0', '\0'};
+    
+    printf("Hex bytes: ");
     for (i = 0; i < sizeof(bytes) / sizeof(int); i++) {
         if (bytes[i] != -1) {
             printf("%x ", bytes[i]);
+            byte_chars[i] = bytes[i];
         }
     }
     printf("\n");
+    printf("Character: %s\n", byte_chars);
 
     return 0;    
-}
-
-void printbitssimple(int n) {
-    unsigned int i;
-
-    i = 1<<(sizeof(n) * 8 - 1);
-
-    while (i > 0) {
-        if (n & i)
-            printf("1");
-        else
-            printf("0");
-        i >>= 1;
-    }
 }
